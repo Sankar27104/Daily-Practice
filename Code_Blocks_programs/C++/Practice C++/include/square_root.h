a6ccb6e55ae3f507e943bafad26314c900415c26
Sankar <sankaranagabalaji@gmail.com>
2024-08-06 21:38:05 +0530

    C++

---
+++ Code_Blocks_programs/C++/Practice C++/include/square_root.h
@@ -0,0 +1,16 @@
+#ifndef SQUARE_ROOT_H
+#define SQUARE_ROOT_H
+
+
+class square_root
+{
+    public:
+        square_root();
+        virtual ~square_root();
+
+    protected:
+
+    private:
+};
+
+#endif // SQUARE_ROOT_H
---
+++ Code_Blocks_programs/C++/Practice C++/src/square_root.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int square_root(){
+    int n=64;
+    cout<<sqrt(n);
+
+    return 0;
+}

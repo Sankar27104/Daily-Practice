15c9f003baa85253db0062e088a7a8dfa837ad5f
Sankar <sankaranagabalaji@gmail.com>
2024-08-19 20:30:23 +0530

    Multiplication table using while loop

--- c_programs/multiplication_while.c
+++ c_programs/multiplication_while.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 int main(){
     int n,result;
+    int i=1;
     printf("Enter the number: ");
     scanf("%d",&n);
-    
-    for(int i=1;i<=10;i++){
+    while(i<=10){
         result=n*i;
         printf("%d * %d = %d\n",n,i,result);
+        i++;
     }
     return 0;
 }

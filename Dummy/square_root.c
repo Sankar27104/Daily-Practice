a4b315d6eaa57c357e1f42c6a5391728a759fa41
Sankar <sankaranagabalaji@gmail.com>
2024-08-21 23:34:52 +0530

    Square root Test

---
+++ Dummy/square_root.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main(){
+    int number;
+    int sr;
+    printf("Enter a number: ");
+    scanf("%d",&number);
+    sr=sqrt(number);
+    printf("Square root of the given number is %d",sr);
+}

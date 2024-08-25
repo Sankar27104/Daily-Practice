239b3fd122a6299232899ef5d0a1e1ae772907af
Sankar <sankaranagabalaji@gmail.com>
2024-08-25 23:02:15 +0530

    Converts a decimal number to binary, octal and hexadecimal forms

---
+++ Python programs/decimal_to_binary.py
@@ -0,0 +1,4 @@
+decimal=int(input("Enter the number to convert: "))
+print(bin(decimal)," is the binary form of ",decimal)
+print(oct(decimal)," is the octal form of ",decimal)
+print(hex(decimal)," is the binary form of ",decimal)
